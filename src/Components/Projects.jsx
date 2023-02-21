38cf24c223069cca54f391a9749db3602f93ba0a
Ana Sobrino <anasonu@hotmail.com>
2023-02-21 13:21:02 +0100

    Add new project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -14,14 +14,16 @@ function Projects() {
           <div className="image-container">
             <div className="overlayed-color"></div>
           </div>
-          <h4 className="project-title">The Requesting Partner</h4>
-          <p className="green">Minigame</p>
+          <div>
+            <h4 className="project-title">The Requesting Partner</h4>
+            <p className="green">Minigame</p>
+          </div>
           <p className="description opacity">
             An easy yet fun minigame in which you have to run away from your
             partner, avoiding obstacles, in order to save yourself of doing
             chores.
           </p>
-          <p className="green">HTML, CSS, JavaScript</p>
+          <p className="green stack">HTML, CSS, JavaScript</p>
           <div className="project-links">
             <a
               className="icon"
@@ -52,14 +54,16 @@ function Projects() {
           <div className="image-container">
             <div className="overlayed-color"></div>
           </div>
-          <h4 className="project-title">Top-Games Portal</h4>
-          <p className="green">CRUD Application</p>
+          <div>
+            <h4 className="project-title">Top-Games Portal</h4>
+            <p className="green">CRUD Application</p>
+          </div>
           <p className="description opacity">
             A portal that contains every minigame developed by my collegues.
             With user authentication and different permissions depending on the
             role.
           </p>
-          <p className="green">
+          <p className="green stack">
             NodeJS, ExpressJS, MongoDB, Mongoose, Handlebars
           </p>
           <div className="project-links">
@@ -92,14 +96,17 @@ function Projects() {
           <div className="image-container">
             <div className="overlayed-color"></div>
           </div>
-          <h4 className="project-title">byChapters</h4>
-          <p className="green">CRUD Application</p>
+          <div>
+            <h4 className="project-title">byChapters</h4>
+            <p className="green">CRUD Application</p>
+          </div>
           <p className="description opacity">
-            A portal where writers can upload their own novels/stories by
-            chapters. Users can read them, mark as favorites and share their
-            favorite chapters. It has a token based authentication.
+            A portal with token based authentication where writers can upload their own stories by
+            chapters. Users can read them, mark as favorites and comment.
           </p>
-          <p className="green">ReactJS, NodeJS, ExpressJS, MongoDB, Mongoose</p>
+          <p className="green stack">
+            ReactJS, NodeJS, ExpressJS, MongoDB, Mongoose
+          </p>
           <div className="project-links">
             <a
               className="icon"
@@ -123,6 +130,41 @@ function Projects() {
                 alt="External link"
               />
             </a>
+          </div>
+        </div>
+
+        <div className="project project4">
+          <div className="image-container">
+            <div className="overlayed-color"></div>
+          </div>
+          <div>
+            <h4 className="project-title">Chromegotchi</h4>
+            <p className="green">Chrome Extension</p>
+          </div>
+          <p className="description opacity">
+            A Chrome extension that contains a virtual pet. It eats, poops and
+            gets sad. Not at all a productivity tool. It's more like a huge
+            distraction... But it's fun anyway.
+          </p>
+          <p className="green stack">ReactJS, Chrome Storage</p>
+          <div className="project-links">
+            <a
+              className="icon"
+              href="https://github.com/anasonu/chromegotchi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={process.env.PUBLIC_URL + "/github-icon.svg"}
+                alt="Github"
+              />
+            </a>
+            {/* <a className="icon" href="*" target="_blank" rel="noreferrer">
+              <img
+                src={process.env.PUBLIC_URL + "/external-link.svg"}
+                alt="External link"
+              />
+            </a> */}
           </div>
         </div>
       </div>
